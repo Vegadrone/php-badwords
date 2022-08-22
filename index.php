d59226fb20f0ebb142399b94bdfb9e0d5f9c9c51
Vegadrone <spenaantonino@gmail.com>
2022-08-22 14:53:00 +0200

    change "testo" to "text"

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-    $testo = 'In una terra lontana, dietro le montagne Parole, lontani dalle terre di Vocalia e Consonantia, 
+    $text= 'In una terra lontana, dietro le montagne Parole, lontani dalle terre di Vocalia e Consonantia, 
     vivono i testi casuali. Vivono isolati nella cittadina di Lettere, sulle coste del Semantico, 
     un immenso oceano linguistico. Un piccolo ruscello chiamato Devoto Oli attraversa quei luoghi, 
     rifornendoli di tutte le regolalie di cui hanno bisogno. È una terra paradismatica, 
@@ -25,7 +25,7 @@
     <h1>PHP Badwords</h1>
     <p>
         <?php 
-            echo $testo
+            echo $text
         ?>
     </p>
 </body>
